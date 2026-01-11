59b8895013ad2910aa590e14827ecfa666924dd1
LerkOFF <lerk@joulerk.ru>
2026-01-11 22:43:02 +0300

    remove: удалены ненужные команды и "приколы"

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import discord
-from dotenv import load_dotenv
-from config import TOKEN, GUILD_IDS, ROLE_ID_TO_MENTION, RESPOND_CHANNEL_IDS
-from insults import check_insults, load_phrases
-from user_commands import my_ckey, give_role, remove_role, make_roles_file, change_my_name_color, add_disposable
+from config import TOKEN, GUILD_IDS
+from user_commands import my_ckey, change_my_name_color, add_disposable
 from role_events import on_member_update
-import re
 
 intents = discord.Intents.default()
 intents.members = True
 intents.guilds = True
-intents.messages = True
-intents.message_content = True
 
 bot = discord.Bot(intents=intents)
-
-WHO_TO_INSULT = load_phrases('phrases/who.txt')
-HOW_TO_INSULT = load_phrases('phrases/how.txt')
 
 
 @bot.event
@@ -31,40 +23,10 @@ bot.slash_command(
     guild_ids=GUILD_IDS
 )(change_my_name_color)
 bot.slash_command(
-    name='give_role',
-    description='Назначить роль пользователю по нику и ID роли',
-    guild_ids=GUILD_IDS
-)(give_role)
-bot.slash_command(
-    name='remove_role',
-    description='Удалить роль у пользователя по нику и ID роли',
-    guild_ids=GUILD_IDS
-)(remove_role)
-bot.slash_command(
-    name='make_roles_file',
-    description='Создать файл с ID и названиями всех ролей',
-    guild_ids=GUILD_IDS
-)(make_roles_file)
-bot.slash_command(
     name='add_disposable',
     description='Добавить токены пользователю по его ckey.',
     guild_ids=GUILD_IDS
 )(add_disposable)
-
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.channel.id in RESPOND_CHANNEL_IDS:
-        if re.search(r'\bкогда\b(?!\-|\w)', message.content.lower()):
-            await message.reply("Завтра")
-
-    if check_insults(message, WHO_TO_INSULT, HOW_TO_INSULT):
-        role = message.guild.get_role(ROLE_ID_TO_MENTION)
-        if role:
-            await message.reply(f"{role.mention}, сообщение содержит запрещенную фразу!")
 
 
 bot.event(on_member_update)
--- user_commands.py
+++ user_commands.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 import discord
 from discord import Option
-from config import TRACKED_ROLES, CKEY_CHANNEL_ID, SPONSORS_FILE_PATH, CAN_GIVES_ROLES, ROLE_GIVER_CHANNEL, \
-    DISPOSABLE_FILE_PATH
+from config import TRACKED_ROLES, CKEY_CHANNEL_ID, SPONSORS_FILE_PATH, CAN_GIVES_ROLES, DISPOSABLE_FILE_PATH
 import re
 from logger import log_user_action
 
@@ -152,79 +151,6 @@ async def change_my_name_color(ctx: discord.ApplicationContext, color_hex: Optio
     except Exception as e:
         await ctx.respond(f"Произошла ошибка при изменении цвета имени: {e}", ephemeral=True)
         log_user_action(f'Error changing color: {e}', member)
-
-
-async def check_permissions_and_find_member_role(ctx, nickname, role_id):
-    if ctx.channel.id != ROLE_GIVER_CHANNEL:
-        return None, None, "Команда вызвана не в разрешённом канале."
-
-    if ctx.author.name not in CAN_GIVES_ROLES:
-        return None, None, "У вас нет прав на выполнение этой команды."
-
-    member = discord.utils.get(ctx.guild.members, name=nickname)
-    if member is None:
-        return None, None, f"Пользователь с ником {nickname} не найден."
-
-    role = ctx.guild.get_role(int(role_id))
-    if role is None:
-        return None, None, f"Роль с ID {role_id} не найдена."
-
-    return member, role, None
-
-
-async def give_role(ctx: discord.ApplicationContext, nickname: Option(str, "Ник пользователя"),
-                    role_id: Option(str, "ID роли")):
-    member, role, error = await check_permissions_and_find_member_role(ctx, nickname, role_id)
-
-    if error:
-        await ctx.respond(error, ephemeral=True)
-        log_user_action(f'Give role failed: {error}', ctx.author)
-        return
-
-    try:
-        await member.add_roles(role)
-        await ctx.respond(f"Пользователю {nickname} успешно назначена роль {role.name}.", ephemeral=True)
-        log_user_action(f'Role {role.name} given to {nickname}', ctx.author)
-    except Exception as e:
-        await ctx.respond(f"Произошла ошибка при назначении роли: {e}", ephemeral=True)
-        log_user_action(f'Error giving role: {e}', ctx.author)
-
-
-async def remove_role(ctx: discord.ApplicationContext, nickname: Option(str, "Ник пользователя"),
-                      role_id: Option(str, "ID роли")):
-    member, role, error = await check_permissions_and_find_member_role(ctx, nickname, role_id)
-
-    if error:
-        await ctx.respond(error, ephemeral=True)
-        log_user_action(f'Remove role failed: {error}', ctx.author)
-        return
-
-    try:
-        await member.remove_roles(role)
-        await ctx.respond(f"У пользователя {nickname} успешно удалена роль {role.name}.", ephemeral=True)
-        log_user_action(f'Role {role.name} removed from {nickname}', ctx.author)
-    except Exception as e:
-        await ctx.respond(f"Произошла ошибка при удалении роли: {e}", ephemeral=True)
-        log_user_action(f'Error removing role: {e}', ctx.author)
-
-
-async def make_roles_file(ctx: discord.ApplicationContext):
-    try:
-        if ctx.author.name not in CAN_GIVES_ROLES:
-            await ctx.respond("У вас нет прав на выполнение этой команды.", ephemeral=True)
-            return
-
-        roles_info = [f"{role.id}: {role.name}" for role in ctx.guild.roles]
-
-        with open("roles_id.txt", "w", encoding="utf-8") as f:
-            f.write("\n".join(roles_info))
-
-        await ctx.respond("Файл roles_id.txt успешно создан.", ephemeral=True)
-        log_user_action('Make roles file command used', ctx.author)
-
-    except Exception as e:
-        await ctx.respond(f"Произошла ошибка при создании файла: {e}", ephemeral=True)
-        log_user_action(f'Error creating roles file: {e}', ctx.author)
 
 
 async def add_disposable(ctx: discord.ApplicationContext, ds_nickname: Option(str, "Дискорд никнейм пользователя"),
